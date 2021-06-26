3ad84629d2673c38ebddc45ae473913200c76052
Alejandro Nanez <alejonanez@gmail.com>
2021-06-26 17:46:45 -0500

    Fetching coins from a 3rd party api

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import {API} from "aws-amplify";
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState({limit: 5, start: 0});
   const [coins, setCoins] = useState([]);
 
+  function updateInputValues(type, value) {
+    setInput({...input, [type]: value});
+  }
+
   async function fetchCoins() {
-    const data = await API.get('cryptoapi', '/coins');
+    const {limit, start} = input;
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
     setCoins(data.coins);
   }
 
-  useEffect(() => {
-    fetchCoins();
-  }, []);
-
   return (
     <div className="App">
+      <input type="text" onChange={(e) => updateInputValues('limit', e.target.value)} placeholder="limit"/>
+      <input type="text" onChange={(e) => updateInputValues('start', e.target.value)} placeholder="start"/>
+      <button onClick={fetchCoins}>Fetch coins</button>
       {
         coins.map(coin => (
           <div key={coin.symbol}>
